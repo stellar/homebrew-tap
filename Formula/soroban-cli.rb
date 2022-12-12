521537d90c37a99e69c438647052001a79cc2558
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-12-12 13:52:20 -0800

    Add CI, and formulas for soroban-cli, stellar-xdr, and stellar-core on futurenet (#1)

---
+++ Formula/soroban-cli.rb
@@ -0,0 +1,18 @@
+class SorobanCli < Formula
+  desc "CLI for interacting with Soroban"
+  homepage "https://soroban.stellar.org"
+  url "https://github.com/stellar/soroban-tools.git", tag: "v0.3.3"
+  head "https://github.com/stellar/soroban-tools.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    chdir "cmd/soroban-cli" do
+      system "cargo", "install", "--bin=soroban", *std_cargo_args
+    end
+  end
+
+  test do
+    assert_match "soroban 0.3.3", shell_output("#{bin}/soroban version")
+  end
+end
---
+++ Formula/stellar-xdr.rb
@@ -0,0 +1,16 @@
+class StellarXdr < Formula
+  desc "CLI for encoding/decoding Stellar XDR"
+  homepage "https://github.com/stellar/rs-stellar-xdr"
+  url "https://github.com/stellar/rs-stellar-xdr.git", tag: "v0.0.11"
+  head "https://github.com/stellar/rs-stellar-xdr.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", "--features=cli", *std_cargo_args
+  end
+
+  test do
+    assert_match "stellar-xdr 0.0.11", shell_output("#{bin}/stellar-xdr version")
+  end
+end

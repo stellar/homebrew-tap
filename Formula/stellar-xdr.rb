8af1e9380c9e98ed51a837b1872f3e865bc8763c
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-12-14 21:49:04 -0800

    Update stellar-xdr.rb

--- Formula/stellar-xdr.rb
+++ Formula/stellar-xdr.rb
@@ -4,12 +4,6 @@ class StellarXdr < Formula
   url "https://github.com/stellar/rs-stellar-xdr.git", tag: "v0.0.11"
   head "https://github.com/stellar/rs-stellar-xdr.git", branch: "main"
 
-  bottle do
-    root_url "https://ghcr.io/v2/stellar/tap"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "dd15d4341de1e6b09f9667b8fafc69f95506db99ff017c532e5a1701618fc321"
-  end
-
   depends_on "rust" => :build
 
   def install

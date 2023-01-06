e476ac4564fffde480c1bdfa40e70f596c20c668
tsachiherman <24438559+tsachiherman@users.noreply.github.com>
2023-01-06 16:42:58 -0500

    update

--- Formula/soroban-cli.rb
+++ Formula/soroban-cli.rb
@@ -1,7 +1,7 @@
 class SorobanCli < Formula
   desc "CLI for interacting with and deploying to Soroban"
   homepage "https://soroban.stellar.org"
-  url "https://github.com/stellar/soroban-tools.git", tag: "v0.3.3"
+  url "https://github.com/stellar/soroban-tools.git", tag: "v0.4.0"
   head "https://github.com/stellar/soroban-tools.git", branch: "main"
 
   depends_on "rust" => :build
@@ -13,6 +13,6 @@ class SorobanCli < Formula
   end
 
   test do
-    assert_match "soroban 0.3.3", shell_output("#{bin}/soroban version")
+    assert_match "soroban 0.4.0", shell_output("#{bin}/soroban version")
   end
 end

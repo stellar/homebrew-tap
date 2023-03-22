bc13fa1fcb7228c78ddbaa16c9916c386813657a
Siddharth Suresh <siddharth@stellar.org>
2023-03-22 12:32:32 -0700

    Bump core

--- Formula/stellar-core-futurenet.rb
+++ Formula/stellar-core-futurenet.rb
@@ -2,7 +2,7 @@ class StellarCoreFuturenet < Formula
   desc "Futurenet build of Stellar Core including Soroban"
   homepage "https://github.com/stellar/stellar-core"
   url "https://github.com/stellar/stellar-core.git",
-      revision: "871accefc0c4a703c1321b4f0e48cf6e03b41960"
+      revision: "064a2787acb9e98c70567523785333581ee1ffa4"
   version "0"
 
   keg_only :versioned_formula
@@ -46,7 +46,7 @@ class StellarCoreFuturenet < Formula
   test do
     # To generate this version string, run `git describe --always --dirty --tags` in
     # your stellar-core repo with the released core revision checked out.
-    assert_match "v19.7.0-22-g871accefc", shell_output("#{bin}/stellar-core version")
+    assert_match "v19.8.0-35-g064a2787a", shell_output("#{bin}/stellar-core version")
     assert_match "soroban-env-host", shell_output("#{bin}/stellar-core version")
     assert_match "Secret seed", shell_output("#{bin}/stellar-core gen-seed")
   end

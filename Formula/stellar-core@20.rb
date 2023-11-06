fb69b77ec8bc25615d1899ef85cdfa471eecfd71
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2023-11-06 12:34:19 -0800

    add comment

--- Formula/stellar-core@20.rb
+++ Formula/stellar-core@20.rb
@@ -1,3 +1,40 @@
+# This formula was copied from the homebrew-core repository and modified:
+#
+# https://github.com/Homebrew/homebrew-core/blob/a00e09fc4f696a8093708e90cb0f7f7c3b54619f/Formula/s/stellar-core.rb
+#
+# Modifications include:
+#
+# - Removing the head.
+# - Removing the bottles.
+# - Adding the depends_on coteutils
+#
+# License from homebrew-core repository:
+#
+# BSD 2-Clause License
+#
+# Copyright (c) 2009-present, Homebrew contributors
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 class StellarCoreAT20 < Formula
   desc "Backbone of the Stellar (XLM) network"
   homepage "https://www.stellar.org/"

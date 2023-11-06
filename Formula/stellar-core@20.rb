62a36e3684820cc694bf6fe6c473be294f45ff2e
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2023-11-06 10:56:26 -0800

    format

--- Formula/stellar-core@20.rb
+++ Formula/stellar-core@20.rb
@@ -1,52 +1,52 @@
 class StellarCoreAT20 < Formula
-    desc "Backbone of the Stellar (XLM) network"
-    homepage "https://www.stellar.org/"
-    url "https://github.com/stellar/stellar-core.git",
-        tag:      "v20.0.0-rc.2.2",
-        revision: "8344f3369493bd4c89796dbb9f0f7aabade7e951"
-    license "Apache-2.0"
+  desc "Backbone of the Stellar (XLM) network"
+  homepage "https://www.stellar.org/"
+  url "https://github.com/stellar/stellar-core.git",
+      tag:      "v20.0.0-rc.2.2",
+      revision: "8344f3369493bd4c89796dbb9f0f7aabade7e951"
+  license "Apache-2.0"
 
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "bison" => :build # Bison 3.0.4+
-    depends_on "libtool" => :build
-    depends_on "pandoc" => :build
-    depends_on "pkg-config" => :build
-    depends_on "libpq"
-    depends_on "libpqxx"
-    depends_on "libsodium"
-    depends_on macos: :catalina # Requires C++17 filesystem
-    depends_on "rust"
-    uses_from_macos "flex" => :build
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "bison" => :build # Bison 3.0.4+
+  depends_on "libtool" => :build
+  depends_on "pandoc" => :build
+  depends_on "pkg-config" => :build
+  depends_on "libpq"
+  depends_on "libpqxx"
+  depends_on "libsodium"
+  depends_on macos: :catalina # Requires C++17 filesystem
+  depends_on "rust"
+  uses_from_macos "flex" => :build
 
-    on_linux do
-      depends_on "libunwind"
-    end
-
-    # https://github.com/stellar/stellar-core/blob/master/INSTALL.md#build-dependencies
-    fails_with :gcc do
-      version "7"
-      cause "Requires C++17 filesystem"
-    end
-
-    def install
-      system "./autogen.sh"
-      system "./configure", "--disable-debug",
-                            "--disable-dependency-tracking",
-                            "--disable-silent-rules",
-                            "--prefix=#{prefix}",
-                            "--enable-postgres"
-      system "make", "install"
-    end
-
-    test do
-      test_categories = %w[
-        accountsubentriescount
-        bucketlistconsistent
-        topology
-        upgrades
-      ]
-      system "#{bin}/stellar-core", "test",
-        test_categories.map { |category| "[#{category}]" }.join(",")
-    end
+  on_linux do
+    depends_on "libunwind"
   end
+
+  # https://github.com/stellar/stellar-core/blob/master/INSTALL.md#build-dependencies
+  fails_with :gcc do
+    version "7"
+    cause "Requires C++17 filesystem"
+  end
+
+  def install
+    system "./autogen.sh"
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}",
+                          "--enable-postgres"
+    system "make", "install"
+  end
+
+  test do
+    test_categories = %w[
+      accountsubentriescount
+      bucketlistconsistent
+      topology
+      upgrades
+    ]
+    system "#{bin}/stellar-core", "test",
+      test_categories.map { |category| "[#{category}]" }.join(",")
+  end
+end

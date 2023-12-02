1c3e97ee23da3ea8d6b5c08473f425e2e052cf4b
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2023-12-01 16:55:41 -0800

    Update stellar-xdr to v20

--- Formula/stellar-xdr.rb
+++ Formula/stellar-xdr.rb
@@ -1,16 +1,16 @@
 class StellarXdr < Formula
   desc "CLI for encoding and decoding Stellar XDR"
   homepage "https://github.com/stellar/rs-stellar-xdr"
-  url "https://github.com/stellar/rs-stellar-xdr.git", tag: "v20.0.0-rc1"
+  url "https://github.com/stellar/rs-stellar-xdr.git", tag: "v20.0.0"
   head "https://github.com/stellar/rs-stellar-xdr.git", branch: "main"
 
   depends_on "rust" => :build
 
   def install
-    system "cargo", "install", "--features=cli", *std_cargo_args
+    system "cargo", "install", "--locked", "--features=cli", *std_cargo_args
   end
 
   test do
-    assert_match "stellar-xdr 20.0.0-rc1", shell_output("#{bin}/stellar-xdr version")
+    assert_match "stellar-xdr 20.0.0", shell_output("#{bin}/stellar-xdr version")
   end
 end

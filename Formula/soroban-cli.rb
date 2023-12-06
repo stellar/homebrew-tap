27e81343668d58935e8fa90fc6198e4af0f8f42e
Tyler.S <tyler.sanderson@stellar.org>
2023-12-06 14:19:55 -0800

    Update Soroban CLI to v20.0.0

--- Formula/soroban-cli.rb
+++ Formula/soroban-cli.rb
@@ -1,7 +1,7 @@
 class SorobanCli < Formula
   desc "CLI for interacting with and deploying to Soroban"
   homepage "https://soroban.stellar.org"
-  url "https://github.com/stellar/soroban-tools.git", tag: "v20.0.0-rc.4.2"
+  url "https://github.com/stellar/soroban-tools.git", tag: "v20.0.0"
   head "https://github.com/stellar/soroban-tools.git", branch: "main"
 
   depends_on "rust" => :build
@@ -13,6 +13,6 @@ class SorobanCli < Formula
   end
 
   test do
-    assert_match "soroban 20.0.0-rc.4.2", shell_output("#{bin}/soroban version")
+    assert_match "soroban 20.0.0", shell_output("#{bin}/soroban version")
   end
 end

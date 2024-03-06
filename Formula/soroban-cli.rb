0c029ba3047f375babc4ddc60a193a747c02a85c
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2024-03-05 17:32:52 -0800

    Update soroban-cli version to latest

--- Formula/soroban-cli.rb
+++ Formula/soroban-cli.rb
@@ -1,18 +1,18 @@
 class SorobanCli < Formula
   desc "CLI for interacting with and deploying to Soroban"
   homepage "https://soroban.stellar.org"
-  url "https://github.com/stellar/soroban-tools.git", tag: "v20.1.0"
-  head "https://github.com/stellar/soroban-tools.git", branch: "main"
+  url "https://github.com/stellar/soroban-cli.git", tag: "v20.3.1"
+  head "https://github.com/stellar/soroban-cli.git", branch: "main"
 
   depends_on "rust" => :build
 
   def install
     chdir "cmd/soroban-cli" do
-      system "cargo", "install", "--bin=soroban", *std_cargo_args
+      system "cargo", "install", "--bin=soroban" *std_cargo_args
     end
   end
 
   test do
-    assert_match "soroban 20.1.0", shell_output("#{bin}/soroban version")
+    assert_match "soroban 20.3.1", shell_output("#{bin}/soroban version")
   end
 end

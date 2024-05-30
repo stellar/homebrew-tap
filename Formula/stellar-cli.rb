d07dd48578a840eebd84a31d2539eee86be09490
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2024-05-30 14:01:13 -0700

    tweak

--- Formula/stellar-cli.rb
+++ Formula/stellar-cli.rb
@@ -1,7 +1,7 @@
 class StellarCli < Formula
   desc "CLI for interacting with and deploying to Stellar"
   homepage "https://developers.stellar.org/"
-  url "https://github.com/stellar/stellar-cli.git", tag: "v20.3.4"
+  url "https://github.com/stellar/stellar-cli.git", tag: "v21.0.0"
   head "https://github.com/stellar/stellar-cli.git", branch: "main"
 
   depends_on "rust" => :build
